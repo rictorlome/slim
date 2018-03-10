8eb7f2d91497f0d5a0eac7c058571b914f0e99fa
rictorlome <rictorlome@gmail.com>
2018-03-10 14:07:22 -0500

    Have channel reducer listen to add_channel_to_current user

--- frontend/reducers/channel_reducer.js
+++ frontend/reducers/channel_reducer.js
@@ -1,5 +1,6 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions.js';
-import { RECEIVE_CHANNELS, RECEIVE_CHANNEL, RECEIVE_DM } from '../actions/channel_actions.js'
+import { RECEIVE_CHANNELS, RECEIVE_CHANNEL,
+  RECEIVE_DM, ADD_CHANNEL_TO_CURRENT_USER } from '../actions/channel_actions.js'
 
 import { merge } from 'lodash';
 
@@ -14,6 +15,11 @@ export const channelReducer = (oldState={}, action) => {
       let newObj = {};
       newObj[action.channel.id] = action.channel;
       return merge({},oldState,newObj)
+    case ADD_CHANNEL_TO_CURRENT_USER:
+        let copy = merge({}, oldState)
+        let channel = copy[action.participation.channel_id]
+        channel.member_ids.push(action.participation.member_id)
+        return copy;
     default:
       return oldState
   }
