d533d2074ac0a306092e9b938ebde3a6395611df
rictorlome <rictorlome@gmail.com>
2018-03-11 17:12:04 -0400

    Attempt to configure production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.web_socket_server_url = "wss://slack-imitation.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://slack-imitation.herokuapp.com', 'http://slack-imitation.herokuapp.com']
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
