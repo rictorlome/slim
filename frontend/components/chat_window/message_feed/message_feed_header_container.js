9cdc351935900bf6166a59e8cfa82c547cee546e
rictorlome <rictorlome@gmail.com>
2018-03-15 09:19:12 -0400

    Fix message feed header bug involving author

--- frontend/components/chat_window/message_feed/message_feed_header_container.js
+++ frontend/components/chat_window/message_feed/message_feed_header_container.js
@@ -5,11 +5,13 @@ import { getCurrentChannel, getDMTitleWithoutCU } from '../../../util/selectors'
 import { MessageFeedHeader } from './message_feed_header';
 
 const msp = (state, ownProps) => {
+  let author;
   let channel = getCurrentChannel(state, ownProps)
+  channel.title === "general" ? author = "admin" : author = state.entities.users[channel.creator_id].username
   return {
     is_dm: channel.is_dm,
     users_minus_you: getDMTitleWithoutCU(state, channel.id),
-    author: state.entities.users[channel.creator_id].username,
+    author: author,
     created_date: channel.created_at,
     title: channel.title
   }
