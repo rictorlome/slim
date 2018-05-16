66755f168d29145c9ce4631079a5ecb1f4774cbf
rictorlome <rictorlome@gmail.com>
2018-05-16 16:36:03 -0400

    Set v2 credentials to hash with env variables

--- app/jobs/query_slimbot_job.rb
+++ app/jobs/query_slimbot_job.rb
@@ -12,10 +12,22 @@ class QuerySlimbotJob < ApplicationJob
   private
 
   def response_from_dflow(message)
+    k = {
+      "type": "service_account",
+      "project_id": ENV['dflow_project_id'],
+      "private_key_id": ENV['dflow_private_key_id'],
+      "private_key": ENV['dflow_private_key'],
+      "client_email": ENV['dflow_client_email'],
+      "client_id": ENV['dflow_client_id'],
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://accounts.google.com/o/oauth2/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-rpfluq%40slimbot-e6c6a.iam.gserviceaccount.com"
+    }
+
     session_code = 'session_number_' + message.id.to_s
-    key_file = 'slimbot-420bc02053ca.json'
-    session_client = Google::Cloud::Dialogflow::V2::Sessions.new credentials: key_file
-    formatted_session = Google::Cloud::Dialogflow::V2::SessionsClient.session_path('slimbot-e6c6a', session_code)
+    session_client = Google::Cloud::Dialogflow::V2::Sessions.new credentials: k
+    formatted_session = Google::Cloud::Dialogflow::V2::SessionsClient.session_path(ENV['dflow_project_id'], session_code)
     last_message = message.body
     query_text = Google::Cloud::Dialogflow::V2::TextInput.new text: last_message, language_code: 'en'
     query_input = Google::Cloud::Dialogflow::V2::QueryInput.new text: query_text
